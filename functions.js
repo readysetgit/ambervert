2957c28350e8dd3d6e72520f4716866c1770f74c
Fahad <fahadfile@gmail.com>
2022-08-15 15:44:28 -0400

    Added image file

---
+++ functions.js
@@ -0,0 +1,7 @@
+function wakeMeUp() {
+    alert("WAKE THE FUCK UP")
+}
+
+function takeMeToIndyGov() {
+    location.href = "https://www.indy.gov/activity/benchmarking-and-transparency#:~:text=Indy's%20benchmarking%20program%2C%20referred%20to,air%20quality%20for%20Indianapolis%20residents."
+}
